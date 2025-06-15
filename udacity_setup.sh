578d2fe4fe22a4635da3901eb49eaa08cf256fb6
Blake Swadling <blake@thecodemonkey.com.au>
2025-06-15 14:49:49 +1000

    fixed up copy paste error in udacity_setup.sh

--- udacity_setup.sh
+++ udacity_setup.sh
@@ -2,7 +2,7 @@
 # author: Blake Swadling
 
 # run this script in your base projects folder to set up the environment
-# for the Udacity C++ Route Planning project
+# for the Udacity C++ System Monitor project
 # this script is for Ubuntu 22.04, it may work on other versions but has not been tested
 # usage: ./udacity_setup.sh
 set -e  # exit on error
@@ -13,7 +13,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y build-essential libcairo2-dev libgraphicsmagick1-dev libpng-dev cmake
 sudo apt install -y libncurses5-dev libncursesw5-dev
 
-#clone the skeleton route planner project
+#clone the skeleton project
 git clone https://github.com/udacity/CppND-System-Monitor.git --recurse-submodules
 
 cd CppND-System-Monitor
@@ -23,9 +23,7 @@ git remote rename origin udacity
 git remote add origin git@github.com:thecodemonkeyau/system-monitor.git
 git fetch origin && git rebase origin/master
 
-# build the sample
-#mkdir -p build && cd build
-#pwd
+# uses make to call cmake
 make build
 
 # run the sample code
